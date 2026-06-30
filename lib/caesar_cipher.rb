edad965b331849003230ad8e583ab085e6b4bae5
smallerchaos <257726345+smallerchaos@users.noreply.github.com>
2026-06-30 14:48:55 +0200

    Following an RSpec tutorial I moved my code into a separate folder. This probably follows more established code architecture patterns.

---
+++ lib/caesar_cipher.rb
@@ -0,0 +1,10 @@
+def encode_to_caesar_cipher(string, number)
+  print "Khoor Zruog!"
+  return "Khoor Zruog!"
+end
+# strip! to remove trailing and leading whitespace
+ 
+#  if message.is_a? String == true && message.ascii_only? == true
+  # else
+  #   return "This isn't a string and cannot be encoded into a cipher."
+  # end
